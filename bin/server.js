2045d50e0291d2e593a453c065fae10661730922
jieyuecoding <jieyuecoding@vip.qq.com>
2023-04-22 11:27:50 +0800

    delete homepage

--- bin/server.js
+++ bin/server.js
@@ -245,17 +245,17 @@ function filterClientOptions(inputOptions, clientToUseForMessage) {
     return outputOptions;
 }
 
-// 定制
-import * as util from 'util';
-// index.html
-server.get('/', async (request, reply) => {
-    try {
-        const readFile = util.promisify(fs.readFile);
-        const send = await readFile('./bin/html/index.html', 'utf-8');
-        reply.raw.setHeader('content-type', 'text/html; charset=utf-8');
-        reply.send(send);
-    } catch (err) {
-        console.log('Error', err);
-    }
-});
-console.log('http://localhost:3000');
+// // 定制
+// import * as util from 'util';
+// // index.html
+// server.get('/', async (request, reply) => {
+//     try {
+//         const readFile = util.promisify(fs.readFile);
+//         const send = await readFile('./bin/html/index.html', 'utf-8');
+//         reply.raw.setHeader('content-type', 'text/html; charset=utf-8');
+//         reply.send(send);
+//     } catch (err) {
+//         console.log('Error', err);
+//     }
+// });
+// console.log('http://localhost:3000');
